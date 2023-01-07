f8f806c34840a62928d8656df60c7123f72c04b8
arthagp <arthagusfi11@gmail.com>
2023-01-07 12:15:16 +0700

    updated fix

--- app.js
+++ app.js
@@ -1,74 +1,71 @@
+const pokemonContainer = document.querySelector(".pokemon-container");
+
+
 async function fetchData(id) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`,{method:'GET'})
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`,
+        { method: 'GET' })
     const data = await response.json();
-    // console.log(data)
-    let numbers = '';
-    // let name = '';
-    let img = '';
-    let type = '';
-    for (let i = 1; i <= data; i++) {
-        // name = name + `${data.name}`
-        
-        
+
+    const cardContainer = document.createElement("div");
+    cardContainer.classList.add("card-container");
+
+    const card = document.createElement("div");
+    card.classList.add("pokemon-block");
+
+    const spriteContainer = document.createElement("div");
+    spriteContainer.classList.add("img-container");
+
+    const sprite = document.createElement("img");
+    sprite.src = data.sprites.front_default;
+
+    spriteContainer.appendChild(sprite);
+
+    const number = document.createElement("p");
+    number.textContent = `#${data.id.toString().padStart(3, 0)}`;
+
+    const name = document.createElement("p");
+    name.classList.add("name");
+    name.textContent = data.name;
+
+    const type = document.createElement('p');
+    type.textContent = `type : ${data.types[0].type.name}`;
+
+    card.appendChild(number);
+    card.appendChild(name);
+    card.appendChild(spriteContainer);
+    card.appendChild(type);
+
+    cardContainer.appendChild(card);
+    document.getElementById('pokemon-container').appendChild(cardContainer);
+    cardContainer.style.cssText = "text-align: center;";
+    number.style.padding ="12px 0 0 0"
+    type.style.padding ="0 0 12px 0"
+    cardContainer.style.margin = "0 500px"
+    card.style.color = "#B1B3B1";
+    if (data.types[0].type.name === 'grass') {
+        card.style.backgroundColor = "#098A03"
+    } else if (data.types[0].type.name === 'fire') {
+        card.style.backgroundColor = "#EB9207"
+    } else if (data.types[0].type.name === 'water') {
+        card.style.backgroundColor = "#0200F0"
+    } else if (data.types[0].type.name === 'bug') {
+        card.style.backgroundColor = "#767376"
+    } else if (data.types[0].type.name === 'normal') {
+        card.style.backgroundColor = "#EAECD2"
+    } else if (data.types[0].type.name === 'poison') {
+        card.style.backgroundColor = "#000050"
+    } else if (data.types[0].type.name === 'electric') {
+        card.style.backgroundColor = "yellow"
+    } else if (data.types[0].type.name === 'ground') {
+        card.style.backgroundColor = "#4E4C4E"
     }
-    document.getElementById("header").insertAdjacentHTML('beforebegin',
-    `<p>${data.id}</p>
-    <p>${data.name}</p>
-    <img src="${data.sprites.front_default}">
-    <p>${data.types[0].type.name}</p>`
-    );
-
-    if (data.types[0].type.name == 'grass') {
-        return true
-    }else{
-        document.getElementById("card").style.cssText = "background-color: green"
-    }
-    // console.log(data.types[0].type)
-
-
 }
 
 function getPokemons(number) {
-    
+
     for (let i = 1; i <= number; i++) {
         fetchData(i);
 
     }
-    // console.log(data)
 }
 
-function createPokemon(pokemon) {
-    const flipCard = document.createElement("div");
-    flipCard.classList.add("flip-card");
-  
-    const cardContainer = document.createElement("div");
-    cardContainer.classList.add("card-container");
-  
-    flipCard.appendChild(cardContainer);
-  
-    const card = document.createElement("div");
-    card.classList.add("pokemon-block");
-  
-    const spriteContainer = document.createElement("div");
-    spriteContainer.classList.add("img-container");
-  
-    const sprite = document.createElement("img");
-    sprite.src = pokemon.sprites.front_default;
-  
-    spriteContainer.appendChild(sprite);
-  
-    const number = document.createElement("p");
-    number.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
-  
-    const name = document.createElement("p");
-    name.classList.add("name");
-    name.textContent = pokemon.name;
-  
-    card.appendChild(spriteContainer);
-    card.appendChild(number);
-    card.appendChild(name);
-  
-    cardContainer.appendChild(card);
-  }
-
-getPokemons(5)
